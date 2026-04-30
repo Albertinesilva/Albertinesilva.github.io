b52d4296b2d230c008219aaba56e7b58c999d789
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-04-30 10:37:03 -0300

    Package.json e vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Albertinesilva.github.io/",
+  base: "/",
   test: {
     environment: "jsdom",
     globals: true,
